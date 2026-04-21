b60cd157ce203b0cdf88edaaed9c4d28af434f0c
Riju Vijayan <rijuvijayan@gmail.com>
2026-04-21 21:38:42 +0530

    chore: husky configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 export default defineConfig([
   {
-    ignores: ['node_modules', 'dist', 'build', 'coverage', '**/*.min.js'],
+    ignores: ["node_modules", "dist", "build", "coverage", "**/*.min.js"],
   },
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
-    extends: ['js/recommended'],
+    extends: ["js/recommended"],
     languageOptions: { globals: globals.node },
   },
   tseslint.configs.recommended,
